b15e8693c8e2bca255274465b41c861900f00734
Tangvfr <tangv.minecraft@gmail.com>
2019-11-01 10:20:08 +0100

    .

--- src/fr/tangv/processmanagerserver/ProcessManagerServer.java
+++ src/fr/tangv/processmanagerserver/ProcessManagerServer.java
@@ -1,6 +1,5 @@
 package fr.tangv.processmanagerserver;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,14 +8,14 @@ public class ProcessManagerServer {
 
 	public static void main(String args[]) throws IOException {
 		//Process process = Runtime.getRuntime().exec("java -jar spigot-1.14.jar", new String[] {}, new File("C:\\Users\\tangv\\Bureau\\Jeux\\Serveur 1.14\\"));
-		Process process = Runtime.getRuntime().exec("cmd", new String[] {}, new File("C:\\Users\\tangv\\Bureau\\Jeux\\Serveur 1.14\\"));
+		Process process = Runtime.getRuntime().exec("cmd");//, new String[] {}, new File("C:\\Users\\tangv\\Bureau\\Jeux\\Serveur 1.14\\"));
 		Thread thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
 					InputStream in = process.getInputStream();
 					OutputStream out = System.out;
-					byte[] buffer = new byte[16];
+					byte[] buffer = new byte[1024];
 					int taille;
 					while((taille = in.read(buffer)) != -1) {
 						out.write(buffer, 0, taille);
@@ -28,6 +27,7 @@ public class ProcessManagerServer {
 			}
 		});
 		thread.start();
+		
 	}
 	
 }
