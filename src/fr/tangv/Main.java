0f13aa3f56c0a082a74489c319489d82353f6901
Tangvfr <tangv.minecraft@gmail.com>
2019-11-17 10:58:41 +0100

    path for web

--- src/fr/tangv/Main.java
+++ src/fr/tangv/Main.java
@@ -1,6 +1,5 @@
 package fr.tangv;
 
-import fr.tangv.processmanagerclient.ProcessManagerClient;
 import fr.tangv.processmanagerserver.ProcessManagerServer;
 
 public class Main {
@@ -9,9 +8,10 @@ public class Main {
 	
 	public static void main(String[] args) {
 		if (args.length >= 1 && args[0].equalsIgnoreCase("-server")) {
-			new ProcessManagerServer();
-		} else {
-			new ProcessManagerClient();
+			ProcessManagerServer pms = new ProcessManagerServer();
+			if (args.length >= 2 && args[1].equalsIgnoreCase("-web")) {
+				
+			}
 		}
 	}
 	
