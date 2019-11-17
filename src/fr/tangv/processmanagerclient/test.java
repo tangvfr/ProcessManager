ef5f514a782deb6974d2abc2435d444a943eabd9
Tangvfr <tangv.minecraft@gmail.com>
2019-11-17 11:01:28 +0100

    path web server

--- src/fr/tangv/processmanagerclient/test.java
+++ src/fr/tangv/processmanagerclient/test.java
@@ -31,33 +31,39 @@ public class test {
 							}
 							System.out.println(">:"+data+":<");
 							//traitement text
-							String lineData = data.substring(0, data.indexOf(" HTTP"));
-							int separatorRequet = lineData.indexOf(" ");
-							String typeRequet = lineData.substring(0, separatorRequet);
-							String repRequet = lineData.substring(separatorRequet+1);
-							String hostData = data.substring(data.indexOf("Host: ")+6);
-							String hostRequet = hostData.substring(0, hostData.indexOf("\r\n"));
-							String[] dataData = hostData.split("\n");
-							String dataRequet = dataData[dataData.length-1].replace("\r", "").replace("\n", "");
-							String ipRequet = socket.getInetAddress().getHostAddress();
-							//--------------------------
-							System.out.println(">:");
-							System.out.println("Type: "+typeRequet);
-							System.out.println("Rep: "+repRequet);
-							System.out.println("Host: "+hostRequet);
-							System.out.println("Data: "+dataRequet);
-							System.out.println("Ip: "+ipRequet);
-							System.out.println(":<");
-							//traitement de la requet
-							if (data.startsWith("GET") || data.startsWith("HEAD")) {
-								sendRequet(socket.getOutputStream(),
-										("<html><head><title>Page Test !</title><meta charset=\"UTF-8\"></head><body style=\"background: #3333DD;\"><center> <form method=\"POST\" action=\"test.html\"><label for=\"pseudo\">Entrez un pseudo:</label><input type=\"text\" name=\"usercreat\" id=\"usercreat\"><br><br><label for=\"pswd\">Choisissez un mot de passe: </label><input type=\"password\" name=\"mdpcreat\" id=\"mdpcreat\"><br><br><label for=\"pswd\">Retapez votre mot de passe: </label><input type=\"password\" name=\"mdpcreat2\" id=\"mdpcreat\"><br><br><input type=\"submit\" value=\"Creation du compte !\" name=\"validation\"><h1>Bonsoir maitre</h1></center></body></html>"
-										).getBytes("UTF8"));
-							} else if (data.startsWith("POST")) {
-								sendRequet(socket.getOutputStream(),
-										("<html><head><title>Post fait</title><meta charset=\"UTF-8\"></head><body><center style=\"background: #222222; color: #ff00ff;\"><h1>Test Post: "+repRequet+"</h1><h1>Data send: "+dataRequet+"</h1></center></body></html>"
-										).getBytes("UTF8"));
-							}
+							try {
+								String lineData = data.substring(0, data.indexOf(" HTTP"));
+								int separatorRequet = lineData.indexOf(" ");
+								String typeRequet = lineData.substring(0, separatorRequet);
+								String repRequet = lineData.substring(separatorRequet+1);
+								String hostData = data.substring(data.indexOf("Host: ")+6);
+								String hostRequet = hostData.substring(0, hostData.indexOf("\r\n"));
+								String[] dataData = hostData.split("\n");
+								String dataRequet = dataData[dataData.length-1].replace("\r", "").replace("\n", "");
+								String ipRequet = socket.getInetAddress().getHostAddress();
+								//--------------------------
+								System.out.println(">:");
+								System.out.println("Type: "+typeRequet);
+								System.out.println("Rep: "+repRequet);
+								System.out.println("Host: "+hostRequet);
+								System.out.println("Data: "+dataRequet);
+								System.out.println("Ip: "+ipRequet);
+								System.out.println(":<");
+								//traitement de la requet
+								try {
+									if (data.startsWith("GET") || data.startsWith("HEAD")) {
+										sendRequet(socket.getOutputStream(),
+												("<html><head><title>Page Test !</title><meta charset=\"UTF-8\"></head><body style=\"background: #3333DD;\"><center> <form method=\"POST\" action=\"test.html\"><label for=\"pseudo\">Entrez un pseudo:</label><input type=\"text\" name=\"usercreat\" id=\"usercreat\"><br><br><label for=\"pswd\">Choisissez un mot de passe: </label><input type=\"password\" name=\"mdpcreat\" id=\"mdpcreat\"><br><br><label for=\"pswd\">Retapez votre mot de passe: </label><input type=\"password\" name=\"mdpcreat2\" id=\"mdpcreat\"><br><br><input type=\"submit\" value=\"Creation du compte !\" name=\"validation\"><h1>Bonsoir maitre</h1></center></body></html>"
+												).getBytes("UTF8"));
+									} else if (data.startsWith("POST")) {
+										sendRequet(socket.getOutputStream(),
+												("<html><head><title>Post fait</title><meta charset=\"UTF-8\"></head><body><center style=\"background: #222222; color: #ff00ff;\"><h1>Test Post: "+repRequet+"</h1><h1>Data send: "+dataRequet+"</h1></center></body></html>"
+												).getBytes("UTF8"));
+									}
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+							} catch (Exception e) {}
 							socket.close();
 						} catch (IOException e) {
 							e.printStackTrace();
