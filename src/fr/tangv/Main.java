caac67f434e20c6ecd2d0ca0fbd9f0d0566ad034
tangvfr <44406440+tangvfr@users.noreply.github.com>
2019-11-19 21:44:07 +0100

    default port

--- src/fr/tangv/Main.java
+++ src/fr/tangv/Main.java
@@ -10,9 +10,9 @@ public class Main {
 	public static void main(String[] args) {
 		if (args.length >= 1 && args[0].equalsIgnoreCase("-server")) {
 			ProcessManagerServer pms = new ProcessManagerServer();
-			if (args.length >= 3 && args[1].equalsIgnoreCase("-web")) {
+			if (args.length >= 2 && args[1].equalsIgnoreCase("-web")) {
 				try {
-					int port = Integer.parseInt(args[2]);
+					int port = args.length >= 3 ? Integer.parseInt(args[2]) : 80;
 					WebServer webServer = new WebServer(port, pms);
 					ProcessManagerServer.logger.info("Open WebServer with port \""+port+"\" !");
 				} catch (Exception e) {
