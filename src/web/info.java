51f8b789a6f70ff6545e8f53483d3eb88063798b
tangv <tangv@DenisComputer>
2020-03-08 16:06:57 +0100

    add comment in page.java

--- src/web/info.java
+++ src/web/info.java
@@ -138,7 +138,7 @@ public class info implements ClassPage {
 						@SuppressWarnings("unchecked")
 						Vector<ProcessPlus> listProcess = (Vector<ProcessPlus>) processManager.getListProcess().clone();
 						sortListProcess(listProcess, data.get("sort"));
-						//implement folder sort
+						//implement folder sort and by content searh bar
 						//calc process
 						for (int i = 0; i < listProcess.size(); i++) {
 							ProcessPlus process = listProcess.get(i);
