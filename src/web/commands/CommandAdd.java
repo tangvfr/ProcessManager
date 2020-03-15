e48df658fc18982be298abad2d67b0f4096a61fc
tangv <tangv@DenisComputer>
2020-03-15 23:21:00 +0100

    path checkbox in page info

--- src/web/commands/CommandAdd.java
+++ src/web/commands/CommandAdd.java
@@ -6,11 +6,11 @@ import fr.tangv.processmanager.util.ProcessPlus;
 public class CommandAdd {
 
 	public CommandAdd(String name, String cmd, String folder, String launch, String cmdStop) throws Exception {
-		if (!name.isEmpty() && !launch.isEmpty()) {
+		if (!name.isEmpty()) {
 			ProcessPlus process = new ProcessPlus(name, cmd, folder, "UTF8", Boolean.parseBoolean(launch), cmdStop);
 			Main.processManagerServer.getProcessManager().addProcess(process);
 		} else {
-			throw new Exception("CommandAdd: name or launch is empty");
+			throw new Exception("CommandAdd: name is empty");
 		}
 	}
 	
