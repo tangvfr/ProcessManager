2de611d1baf77381f5e23ca57431ae19e13034b0
tangv <tangv@DenisComputer>
2020-03-19 13:31:37 +0100

    path image, and change text to ondisconnect

--- src/web/js/ondisconnect.js
+++ src/web/js/ondisconnect.js
@@ -1,5 +1,5 @@
 function onDisconnect(username) {
     let list = ["", ""];
-    popMenuCommand.show(true, "Disconnect to "+username, list, token, "disconnect", "POST", "/command.tweb"
+    popMenuCommand.show(true, "Disconnect To "+username, list, token, "disconnect", "POST", "/command.tweb"
     , () => {this.sumbit();}, () => {popMenuCommand.hide();});
 }
